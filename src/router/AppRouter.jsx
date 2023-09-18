abae7f6738e663a74e1b37514fd3c8adb594afca
burhan <alacaburhan122016@gmail.com>
2023-09-18 17:43:20 +0300

    BrowserRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Main from './../pages/Main';
+import Login from './../pages/Login';
+import MovieDetail from './../pages/MovieDetail';
+import Register from './../pages/Register';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return 
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Main/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/details/:id" element={<MovieDetail/>}/>
+    </Routes>
+    
+    </BrowserRouter>;
+    
 };
 
 export default AppRouter;
